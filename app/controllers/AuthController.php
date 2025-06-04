60f2f22bfdd729eb6a723e922dfb3f4bb32059f4
fedor <fedor.dorofeyev@freedompay.money>
2025-06-04 21:34:51 +0500

    master - добавил авторизацию на jwt

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -3,14 +3,49 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Models\Users;
+use Firebase\JWT\JWT;
 use Phalcon\Mvc\Controller;
+
 
 class AuthController extends Controller
 {
     public function loginAction()
     {
-        return $this->response->setStatusCode(200,'ok')->setJsonContent([
-            'message'=> 'ok'
+        $data     = $this->request->getJsonRawBody(true);
+        $email    = trim($data['email'] ?? "");
+        $password = $data['password'] ?? "";
+
+        /** @var \Phalcon\Config\Config $config */
+        $config = $this->config;
+
+        if (!$email || !$password) {
+            return $this->response->setStatusCode(422, "Unprocessable Entity")->setJsonContent([
+                'status'  => 'error',
+                "message" => 'Email and Password required'
+            ]);
+        }
+
+        $user = Users::findFirst(['email' => $email]);
+
+        if (!$user || !$this->security->checkHash($password, $user->getPasswordHash())) {
+            return $this->response->setStatusCode(422, "Unprocessable Entity")->setJsonContent([
+                'status'  => 'error',
+                "message" => 'Email or Password incorrect'
+            ]);
+        }
+
+        $payload = [
+            'email' => $user->getEmail(),
+            'iat'=> time(),
+            'exp' => time() + $config->security->jwtExpire
+        ];
+
+        $jwt = JWT::encode($payload, $config->security->jwtSecret, $config->security->jwtAlgorithm);
+
+        return $this->response->setStatusCode(200, 'ok')->setJsonContent([
+            'status' => 'ok',
+            'jwt'=> $jwt
         ]);
     }
 }
