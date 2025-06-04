657d6d5944cc967963400879e292599e3d0cd1be
fedor <fedor.dorofeyev@freedompay.money>
2025-06-04 15:56:58 +0500

    master - добавил scaffold проекта

--- docker/.Dockerfile
+++ docker/.Dockerfile
@@ -3,6 +3,16 @@ FROM phalconphp/cphalcon:v5.9.2-php8.4
 USER root
 
 RUN apt update && apt install -y nginx && rm  /etc/nginx/sites-enabled/default
+RUN pecl install xdebug && docker-php-ext-enable xdebug
+RUN apt install -y curl wget htop
+RUN php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" \
+    && php ./composer-setup.php \
+    && php -r "unlink('composer-setup.php');" \
+    && mv composer.phar /usr/local/bin/composer
+
+RUN composer global require phalcon/devtools:"^5.0@dev" --dev \
+    && echo 'export PATH="/root/.composer/vendor/bin:$PATH"' >> ~/.bashrc \
+    && source ~/.bashrc
 
 ENTRYPOINT /app/docker/entrypoint.sh
 
--- public/index.php
+++ public/index.php
@@ -1,27 +1,47 @@
 <?php
+declare(strict_types=1);
 
-use Phalcon\Mvc\Application;
 use Phalcon\Di\FactoryDefault;
-use Phalcon\Mvc\View;
-use Phalcon\Mvc\Controller;
 
-class IndexController extends Controller {
-    public function indexAction() {
-        echo "<h1>Hello from Phalcon</h1>";
-    }
+error_reporting(E_ALL);
+
+define('BASE_PATH', dirname(__DIR__));
+define('APP_PATH', BASE_PATH . '/app');
+
+try {
+    /**
+     * The FactoryDefault Dependency Injector automatically registers
+     * the services that provide a full stack framework.
+     */
+    $di = new FactoryDefault();
+
+    /**
+     * Read services
+     */
+    include APP_PATH . '/config/services.php';
+
+    /**
+     * Handle routes
+     */
+    include APP_PATH . '/config/router.php';
+
+    /**
+     * Get config service for use in inline setup below
+     */
+    $config = $di->getConfig();
+
+    /**
+     * Include Autoloader
+     */
+    include APP_PATH . '/config/loader.php';
+
+    /**
+     * Handle the request
+     */
+    $application = new \Phalcon\Mvc\Application($di);
+
+    echo $application->handle($_SERVER['REQUEST_URI'])->getContent();
+} catch (\Exception $e) {
+    echo $e->getMessage() . '<br>';
+    echo '<pre>' . $e->getTraceAsString() . '</pre>';
 }
-
-$di = new FactoryDefault();
-
-$di->set(
-    'view',
-    function () {
-        $view = new View();
-        $view->setViewsDir('../app/views/');
-        return $view;
-    }
-);
-
-$app = new Application($di);
-
-$app->handle($_SERVER["REQUEST_URI"])->send();
