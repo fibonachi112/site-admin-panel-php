d53d1ccab64660520271facaa42343ccb864e238
fedor <fedor.dorofeyev@freedompay.money>
2025-06-04 17:07:12 +0500

    master - название экшена и роутер

--- app/config/router.php
+++ app/config/router.php
@@ -6,16 +6,14 @@ $router = $di->getRouter();
 
 $api = new Group([
     'namespace' => 'App\Controllers',
-    'controller'=> 'Auth',
 ]);
 
-$api->setPrefix("/api");
+$api->setPrefix('/api');
 
-$api->AddPost('/login',[
-    'controller' => 'Auth',
-    'action' => 'login',
+$api->addPost('/login', [
+    'controller' => 'auth',
+    'action'     => 'login',
 ]);
-
 $router->mount($api);
 
 $router->handle($_SERVER['REQUEST_URI']);
