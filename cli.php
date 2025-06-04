0a68398d6b8b95d4438062de3628aed9e01900ba
fedor <fedor.dorofeyev@freedompay.money>
2025-06-05 02:04:33 +0500

    master - переработал cli

--- cli.php
+++ cli.php
@@ -1,10 +1,26 @@
 <?php
 
+use Dotenv\Dotenv;
 use Phalcon\Autoload\Loader;
 use Phalcon\Cli\Console;
 use Phalcon\Cli\Console\Exception as PhalconException;
 use Phalcon\Cli\Dispatcher;
 use Phalcon\Di\FactoryDefault\Cli as CliDI;
+
+error_reporting(E_ALL);
+
+define('BASE_PATH', realpath(__DIR__));
+define('APP_PATH', BASE_PATH . '/app');
+
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(BASE_PATH);
+
+$dotenv->load();
+
+foreach ($_ENV as $key => $value) {
+    putenv("$key=$value");
+}
 
 $loader = new Loader();
 $loader->setNamespaces([
@@ -12,16 +28,35 @@ $loader->setNamespaces([
 ]);
 $loader->register();
 
-$container  = new CliDI();
+$di         = new CliDI();
 $dispatcher = new Dispatcher();
 
 $dispatcher->setDefaultNamespace('App\Tasks');
-$container->setShared('dispatcher', $dispatcher);
-$container->setShared('config', function () {
-    return include 'app/config/config.php';
+$di->setShared('dispatcher', $dispatcher);
+$di->setShared('db', function () {
+    $config = $this->getConfig();
+
+    $class  = 'Phalcon\Db\Adapter\Pdo\\' . $config->database->adapter;
+    $params = [
+        'host'     => $config->database->host,
+        'username' => $config->database->username,
+        'password' => $config->database->password,
+        'dbname'   => $config->database->dbname,
+        'charset'  => $config->database->charset
+    ];
+
+    if ($config->database->adapter == 'Postgresql') {
+        unset($params['charset']);
+    }
+
+    return new $class($params);
 });
 
-$console = new Console($container);
+$di->setShared('config', function () {
+    return include 'app/Config/config.php';
+});
+
+$console = new Console($di);
 
 $arguments = [];
 foreach ($argv as $k => $arg) {
