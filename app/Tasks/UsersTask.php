9cd8f1454fb38e606316660dc9a0c79520091423
fedor <fedor.dorofeyev@freedompay.money>
2025-06-05 02:03:49 +0500

    master - переименовал папки

--- app/Tasks/UsersTask.php
+++ app/Tasks/UsersTask.php
@@ -2,6 +2,8 @@
 
 namespace App\Tasks;
 
+use App\Models\Users;
+use App\Validations\NewUserValidator;
 use Phalcon\Cli\Task;
 
 /**
@@ -11,6 +13,27 @@ class UsersTask extends Task
 {
     public function addAction(array $params)
     {
+        $validator = new NewUserValidator();
+
+        [$email, $password] = $params + [null, null];
+
+        $messages = $validator->validate([
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        if (count($messages) > 0) {
+            foreach ($messages as $message) {
+                echo $message . PHP_EOL;
+            }
+            return;
+        }
+
+        $user = new Users([
+            'email'=> $params[0],
+            'passwordHash'=> $this->security->hash($params[1]),
+        ]);
+
         echo "Создание пользователя...\n";
         var_dump($params);
     }
