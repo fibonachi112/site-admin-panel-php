0534701e9210f2ca68fed03db002632a019db893
fedor <fedor.dorofeyev@freedompay.money>
2025-06-05 17:42:53 +0500

    master - phpdoc

--- app/Controllers/ControllerBase.php
+++ app/Controllers/ControllerBase.php
@@ -3,10 +3,14 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Models\Users;
 use Phalcon\Mvc\Controller;
 
 class ControllerBase extends Controller
 {
+    /**
+     * @return Users
+     */
     protected function getAuthUser()
     {
         return $this->di->getShared('auth_user');
